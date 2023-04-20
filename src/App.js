b7dddf4e877157c65c6efd6fcf53df4d543e0b7a
007lazi@gmail.com <007lazi@gmail.com>
2023-04-20 17:45:37 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+
+
+  function addItem() {
+    if (!newItem) {
+      alert("Press enter an item.");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+
+    // Add new item to items array
+    setItems((oldList) => [...oldList, item]);
+
+    // Reset newItem back to original state
+    setNewItem("");
+  }
+
+  /* Deletes an item based on the `item.id` key */
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>My Todo List</h1>
+
+      <input
+        type="text"
+        placeholder="Add an item..."
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+
+      <button onClick={() => addItem()}>Add</button>
+
+      <ul>
+        {items.map((item) => {
+          return (
+            <div>
+              <li key={item.id}>
+                {item.value}
+                <button
+                  className="delete-button"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  ❌
+                </button>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
